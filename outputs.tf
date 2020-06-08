bddbdb173d9323a9978e17d317586515c5f384e9
omhmichaels <youngswaggod@gmail.com>
2020-06-08 03:30:49 -0700

    Added some Terra 12

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "vnet_id" {
+/*output "vnet_id" {
   description = "The id of the newly created vNet"
   value       = azurerm_virtual_network.vnet.id
-}
+}*/
 
 output "vnet_name" {
   description = "The Name of the newly created vNet"
--- variables.tf
+++ variables.tf
@@ -30,9 +30,5 @@ variable "subnet_names" {
 
 variable "tags" {
   description = "The tags to associate with your network and subnets."
-  type        = map(string)
 
-  default = {
-    environment = "dev"
-  }
 }
